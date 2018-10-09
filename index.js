722539a5ebb9a65f3b60ba0ffc03857d6dfeed20
psvysakh <psvysakh2@gmail.com>
2018-10-09 20:17:48 +0530

    m

---
+++ index.js
@@ -0,0 +1,56 @@
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('status'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'data.json'
+})
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('Animator'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'data.json'
+  })
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('dollars1'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'data2.json'
+  })
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('dollars2'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'data2.json'
+  })
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('dollars3'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'data2.json'
+  })
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('paint'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'brush.json'
+  })
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('laptop'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'laptop.json'
+  })
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('plane'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'plane.json'
+  })
