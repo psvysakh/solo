cb2dc25076d1995a9771a57713dc4add9696b6b1
psvysakh <psvysakh2@gmail.com>
2018-10-09 13:26:43 +0530

    m

---
+++ index.js
@@ -0,0 +1,42 @@
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('preloader'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'data.json'
+})
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('Animator'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'data.json'
+  })
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('dollars1'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'data2.json'
+  })
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('dollars2'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'data2.json'
+  })
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('dollars3'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'data2.json'
+  })
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('paint'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'brush.json'
+  })
